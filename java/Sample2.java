c3b1771f0d0b0677eca966b4d7cc35669252f5be
YOSSHII <mi211304@edu.okinawa-ct.ac.jp>
2024-04-17 10:01:24 +0900

    println2.java

---
+++ java/Sample2.java
@@ -0,0 +1,12 @@
+public class Sample2
+{
+    public static void main(String args[])
+    {
+        int i = 5;
+        int j = 10;
+
+        System.out.printf("%d + %d = %d\n", i, j, i+j);
+
+        System.out.println(i + " + " + j + " = " + (i+j));
+    }
+}
