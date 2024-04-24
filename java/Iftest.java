ca378df7fc8957f2832cf869dfa0ad78610dbfa2
YOSSHII <mi211304@edu.okinawa-ct.ac.jp>
2024-04-24 09:15:46 +0900

    If.java

---
+++ java/Iftest.java
@@ -0,0 +1,19 @@
+public class Iftest
+{
+    public static void main(String args[])
+    {
+        int i;
+
+        i = 0;
+        if(i == 0)
+        {
+            System.out.println("OK = 0");
+        }
+
+        i = 1;
+        if(i == 1)
+        {
+            System.out.println("OK = 1");
+        }
+    }
+}
